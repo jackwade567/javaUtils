a6c6af7cd7a030dd16d068bf983b862fab1628c2
Mohd Nazar Ali Tyagi <nazar@ranktechnologies.com>
2023-11-20 08:32:45 -0500

    new commit

---
+++ transfer/realsogi/EncounterParticipantRepository.java
@@ -0,0 +1,17 @@
+package com.bcbsfl.ccn.realsogi.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.bcbsfl.ccn.realsogi.entites.EncounterParticipant;
+
+
+public interface EncounterParticipantRepository extends JpaRepository<EncounterParticipant, Long> {
+
+	@Query("select r from EncounterParticipant r where LOWER(r.cipId) = LOWER(?1)")
+	Optional<EncounterParticipant> findByCipId(String cipId);
+
+}
